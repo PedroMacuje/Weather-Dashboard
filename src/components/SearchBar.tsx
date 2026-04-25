4beceaf05ab223e9713c68e344285a5a9b75d493
Pedro Macuje <pedro.macuje@usabit.com.br>
2026-04-25 12:40:54 -0300

    style: change search button width

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -30,7 +30,7 @@ export default function SearchBar({ isLoading, onSearch }: SearchBarProps) {
       <button
         type="submit"
         disabled={isLoading}
-        className="px-4 py-2 bg-blue-500 text-white rounded-lg duration-300 
+        className="w-[120px] py-1 bg-blue-500 text-white rounded-lg duration-300 
           ease-in-out hover:bg-blue-600 disabled:bg-gray-400 
           disabled:cursor-not-allowed transition-colors"
       >
