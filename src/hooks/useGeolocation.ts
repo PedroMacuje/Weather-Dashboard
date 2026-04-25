6e628a978fb311451ebd0536f25aa9500be9d601
Pedro Macuje <pedro.macuje@usabit.com.br>
2026-04-25 12:02:10 -0300

    feat: add geolocation hook

---
+++ src/hooks/useGeolocation.ts
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { getWeatherByCoord } from "../services/weatherAPI";
+import type { WeatherData } from "../types";
+
+export function useGeolocation() {
+  const [isLocating, setIsLocating] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  async function getCurrentWeather(): Promise<WeatherData | null> {
+    if (!navigator.geolocation) {
+      setError("Geolocalização não suportada");
+      return null;
+    }
+
+    setIsLocating(true);
+    setError(null);
+
+    return new Promise((resolve) => {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          try {
+            const { latitude, longitude } = position.coords;
+            const data = await getWeatherByCoord(latitude, longitude);
+            resolve(data);
+          } catch (error) {
+            setError("Erro ao buscar geolocalização");
+            resolve(null);
+          } finally {
+            setIsLocating(false);
+          }
+        },
+        () => {
+          setError("permissão negada");
+          setIsLocating(false);
+          resolve(null);
+        },
+      );
+    });
+  }
+}
